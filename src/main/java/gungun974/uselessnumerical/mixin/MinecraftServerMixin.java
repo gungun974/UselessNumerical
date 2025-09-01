7762699b7c229640f4b1d604c1f1fdf03b6ee76b
Gungun974 <xfelix974@gmail.com>
2025-09-01 22:38:00 +0400

    Make server load custom world ids configuration

---
+++ src/main/java/gungun974/uselessnumerical/mixin/MinecraftServerMixin.java
@@ -0,0 +1,42 @@
+package gungun974.uselessnumerical.mixin;
+
+import gungun974.uselessnumerical.MinecraftIdsConfiguration;
+import gungun974.uselessnumerical.UselessNumericalMod;
+import net.minecraft.core.net.PropertyManager;
+import net.minecraft.server.MinecraftServer;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.io.File;
+
+@Mixin(value = MinecraftServer.class, remap = false)
+public class MinecraftServerMixin {
+	@Shadow
+	@Nullable
+	public String argWorld;
+
+	@Shadow
+	public PropertyManager propertyManager;
+
+	@Inject(method = "startServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/data/registry/Registries;<init>()V"))
+	public void beforeGameStartEntrypoint(CallbackInfoReturnable<Boolean> cir){
+		String lName = this.argWorld == null ? this.propertyManager.getStringProperty("level-name", "world") : this.argWorld;
+
+		if (MinecraftIdsConfiguration.getInstance().hasWorldConfiguration(new File(lName))) {
+			MinecraftIdsConfiguration.getInstance().loadWorldConfiguration(new File(lName));
+			UselessNumericalMod.LOGGER.info("Load \"{}\" ids configuration", lName);
+		}
+	}
+
+	@Inject(method = "startServer", at = @At("TAIL"))
+	public void afterGameStartEntrypoint(CallbackInfoReturnable<Boolean> cir){
+		String lName = this.argWorld == null ? this.propertyManager.getStringProperty("level-name", "world") : this.argWorld;
+
+		UselessNumericalMod.LOGGER.info("Save \"{}\" ids configuration", lName);
+		MinecraftIdsConfiguration.getInstance().saveWorldConfiguration(new File(lName));
+	}
+}
